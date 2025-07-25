49cea124d45a7dab443a8aa0a3c94467daa5079b
Lio-Dev <leith34@gmail.com>
2025-07-25 14:59:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { TreePine } from "lucide-react";
+import ProductRow from "./components/product_row";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(`${process.env.API_URL}/products`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  const listProducts = products.map((product) => {
+    return product.variants.map((variant) => (
+      <ProductRow
+        key={variant.id}
+        title={
+          product.title +
+          (variant.title === "Default Title" ? "" : " - " + variant.title)
+        }
+        product={product}
+        variant={variant}
+      />
+    ));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-content">
+          <TreePine />
+          <h1>
+            Inven<span className="highlight">Tree</span>
+          </h1>
+        </div>
       </header>
+      <main className="main-content">
+        <div className="table-wrapper">
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th className="sku-column">Sku</th>
+                <th>Title</th>
+                <th className="stock-column">Stock</th>
+              </tr>
+            </thead>
+            <tbody>{listProducts}</tbody>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/product_row.js
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { CircleAlert, CirclePlus, CircleMinus } from "lucide-react";
+import "./product_row.css";
+
+const ProductRow = ({ title, product, variant }) => {
+  const [quantity, setQuantity] = useState(variant.inventory_quantity);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  /* Function to verify if stock tracking is enabled */
+  const isStockTrackingEnabled = async () => {
+    const response = await fetch(
+      `${process.env.API_URL}/stock/${variant.inventory_item_id}`
+    );
+    const data = await response.json();
+
+    if (data.body.inventory_levels[0].available !== null) {
+      setModalOpen(true);
+    } else {
+      alert("Stock tracking is not enabled for this product.");
+    }
+  };
+
+  const changeQuantityApi = () => {
+    try {
+      fetch(`${process.env.API_URL}/stock/${variant.inventory_item_id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ quantity }),
+      }).then((response) => {
+        if (response.status === 200) {
+          setModalOpen(false);
+          alert("Stock level updated successfully!");
+        }
+      });
+    } catch (error) {
+      console.error("Error updating stock level:", error);
+    }
+  };
+
+  return (
+    <tr>
+      <td>{product.id}</td>
+      <td>{title}</td>
+      <td>
+        {modalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Update Stock Level</h2>
+              <div className="modal-body">
+                <CircleMinus
+                  style={{ cursor: "pointer", marginRight: "6px" }}
+                  color="red"
+                  onClick={() => {
+                    if (quantity > 0) setQuantity(quantity - 1);
+                  }}
+                />
+                <input
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => setQuantity(Number(e.target.value))}
+                  style={{ width: "50px", textAlign: "center" }}
+                />
+                <CirclePlus
+                  style={{ cursor: "pointer", marginLeft: "6px" }}
+                  color="lightgreen"
+                  onClick={() => setQuantity(quantity + 1)}
+                />
+              </div>
+              <div className="modal-actions">
+                <button
+                  className="modal-close"
+                  onClick={() => setModalOpen(false)}
+                >
+                  Close
+                </button>
+                <button
+                  className="modal-update"
+                  onClick={() => {
+                    changeQuantityApi();
+                  }}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="stock-level" onClick={() => isStockTrackingEnabled()}>
+          {quantity}
+          {quantity < 10 && (
+            <CircleAlert style={{ marginLeft: "6px" }} color="red" />
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default ProductRow;
